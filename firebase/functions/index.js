51f31c1e86e0b4221ed9a948e8924e8951450f8e
Kesavan Kushalnagar <kesavan.kushalnagar@gmail.com>
2018-12-13 23:13:14 -0800

    add halve life

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -11,13 +11,25 @@ exports.ygo_calc = functions.https.onRequest((request, response) => {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
+  function welcome(agent){
+    agent.add('Welcome to the Yugioh Calculator for Google assistant!');
+    agent.add(new Suggestion('Start a new duel'));
+  }
+
+  function fallback(agent){
+    agent.add('Sorry I did not understand. Please try again.');
+    agent.add(new Suggestion('Start a new duel'));
+    agent.add(new Suggestion('1000 damage to player 1'));
+    agent.add(new Suggestion('Undo'));
+  }
+
   function startGame(agent) {
     agent.add(`Starting a new duel.`);
     init_game(agent);
     agent.add(makeLifeText(agent));
     agent.add(new Suggestion(`Roll a die`));
-    agent.add(new Suggestion('Deal 1000 damage to player 2'));
-    agent.add(new Suggestion('Player 1 gains 1000 life points'));
+    agent.add(new Suggestion('1000 damage to player 2'));
+    agent.add(new Suggestion('Player 1 gains 1000 life'));
   }
   
   /**
@@ -42,6 +54,9 @@ exports.ygo_calc = functions.https.onRequest((request, response) => {
     player_won ? agent.add(winningText(agent)) : agent.add(makeLifeText(agent));
 
     agent.add('Dealt ' + damage + ' damage to Player ' + player + ".");
+    agent.add(new Suggestion(`Roll a die`));
+    agent.add(new Suggestion('1000 damage to player 1'));
+    agent.add(new Suggestion('Player 1 gains 1000'));
     agent.add(new Suggestion('Undo'));
   }
 
@@ -87,7 +102,11 @@ exports.ygo_calc = functions.https.onRequest((request, response) => {
   
   function rollDice(agent){
     let out = rollDie(agent.parameters.number);
+    agent.add(makeLifeText(agent));
     agent.add('Rolled a ' + out);
+    agent.add('Player '  + player + ' gained ' + gain + " points.");
+    agent.add(new Suggestion(`Roll a die`));
+    agent.add(new Suggestion('1000 damage to player 1'));
   }
   
   function rollDie(sides){
@@ -108,6 +127,8 @@ exports.ygo_calc = functions.https.onRequest((request, response) => {
     setLife(agent, data.p1.life, data.p2.life);
     agent.add(makeLifeText(agent));
     agent.add('Player '  + player + ' gained ' + gain + " points.");
+    agent.add(new Suggestion(`Roll a die`));
+    agent.add(new Suggestion('1000 damage to player 1'));
     agent.add(new Suggestion('Undo'));
   }
 
@@ -140,13 +161,36 @@ exports.ygo_calc = functions.https.onRequest((request, response) => {
       agent.context.set(context);
     }
     agent.add(makeLifeText(agent));
+    agent.add(new Suggestion(`Roll a die`));
+    agent.add(new Suggestion('1000 damage to player 2'));
     agent.add('Undid most recent action');
+  }
+
+  function halveLife(agent){
+    var data = getData(agent);
+    let player = agent.parameters.number;
+    if (player == 1){ 
+        data.p1.life = data.p1.life / 2;
+    }
+    else {
+        data.p2.life -= data.p2.life / 2;
+    }
+    setLife(agent, data.p1.life, data.p2.life);
+
+    agent.add(makeLifeText(agent));
+    agent.add('Halved Player ' + player + "'s life.");
+    agent.add(new Suggestion(`Roll a die`));
+    agent.add(new Suggestion('Player 1 gains 1000'));
+    agent.add(new Suggestion('Undo'));
   }
       
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
   intentMap.set('Play Game', startGame);
   intentMap.set('Player Takes Damage', dealDamageToPlayer);
+  intentMap.set('Halve Life', halveLife);
   intentMap.set('Roll Dice', rollDice);
   intentMap.set('Player Gains Life', playerGainsLife);
   intentMap.set('Undo', undoAction);
